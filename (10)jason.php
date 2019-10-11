44bea6f8465b5cc4ec8b81e3219afe78a8c3160d
DarkCode7dev <55891686+DarkCode7dev@users.noreply.github.com>
2019-10-11 08:40:45 -0700

    Update and rename jason.php to (10)jason.php

--- (10)jason.php
+++ (10)jason.php
@@ -1,5 +1,5 @@
 <?php
 //associative array
 $arr = array("Name"=>"Anoop","Empid"=>271343,"Mobile"=>array("7411","8880"));
-$json=json_encode($arr);//calling array into json
+$json=json_encode($arr);//calling array into json//json_encode — Returns the JSON representation of a value
 echo $json;
